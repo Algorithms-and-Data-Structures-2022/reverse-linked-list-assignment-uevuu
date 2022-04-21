1ff51bcb833215c7cee11b966cd8ae32ae541bb3
Nikita Marin <nikita18.marin@gmail.com>
2022-04-21 22:50:58 +0300

    done linked_list reverse

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -1,6 +1,6 @@
 #include "assignment/linked_list.hpp"
 
-#include <utility> // swap
+#include <utility>  // swap
 
 namespace assignment {
 
@@ -13,6 +13,22 @@ namespace assignment {
    */
   void LinkedList::ReverseIterative() {
     // Напишите здесь свой код ...
+    if (size_ <= 0) {
+      return;
+    }
+    Node* prev_node = nullptr;
+    Node* node = front_;
+    Node* next_node = node->next;
+    while (next_node != nullptr) {
+      node->next = prev_node;
+      prev_node = node;
+      node = next_node;
+      next_node = next_node->next;
+    }
+    node->next = prev_node;
+    Node* front_copy = front_;
+    front_ = back_;
+    back_ = front_copy;
   }
 
   /**
@@ -24,11 +40,24 @@ namespace assignment {
    */
   void LinkedList::ReverseRecursive() {
     // Напишите здесь свой код ...
+    if (size_ <= 0) {
+      return;
+    }
+    reverse_recursive_helper(front_->next, front_);
+    front_->next = nullptr;
+    Node* front_copy = front_;
+    front_ = back_;
+    back_ = front_copy;
   }
 
   // вспомогательный метод для реализации рекурсии
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
     // Напишите здесь свой код ...
+    if (curr == nullptr) {
+      return;
+    }
+    reverse_recursive_helper(curr->next, curr);
+    curr->next = prev;
   }
 
 }  // namespace assignment
